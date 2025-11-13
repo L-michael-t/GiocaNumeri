0bf69edf30fcffc5e088c6df7fdc268c3fb4d661
L-michael-t <michaeltifi1@gmail.com>
2025-11-13 11:46:23 +0000

    seconda versione

--- src/GiocaNumeri.java
+++ src/GiocaNumeri.java
@@ -4,19 +4,22 @@ public class GiocaNumeri {
 
         GestoreGioco gestore = new GestoreGioco();
 
-        Giocatore g1 = new Giocatore("Mario", gestore);
-        Giocatore g2 = new Giocatore("Bros", gestore);
+        Giocatore g1 = new Giocatore("cicalone", gestore);
+        Giocatore g2 = new Giocatore("franchino", gestore);
 
         g1.start();
+        g1.join();
         g2.start();
+        g2.join();
 
-
+/* 
         try {
             g1.join();
             g2.join();
         } catch (InterruptedException e) {
             System.out.println("errore");
         }
+            */
 
         System.out.println("FINE GIOCO!");
     }
