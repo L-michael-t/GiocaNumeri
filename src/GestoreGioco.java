dc2157e724bafe9001dda7464e264eaca475594d
L-michael-t <michaeltifi1@gmail.com>
2025-11-20 11:36:55 +0000

    terza versione

--- src/GestoreGioco.java
+++ src/GestoreGioco.java
@@ -1,20 +1,30 @@
 public class GestoreGioco {
     private final int numeroVincente;
-
+    private String vincitore;
 
     public GestoreGioco() {
         numeroVincente = 19;
+        vincitore = null;
+
     }
 
     public GestoreGioco(GestoreGioco gg){
         numeroVincente = gg.numeroVincente;
+        vincitore = gg.vincitore;
     }
 
 
-    public void verifica(int num) {
-        if (num == numeroVincente) {
+    public synchronized void verifica(int num) {
+        if(vincitore != null) {
+            
+            System.out.println("il gioco è già stato vinto da " + vincitore);
+            return;
+        }
+        else if (num == numeroVincente) {
             //verifica num
             System.out.println("ho vinto bella freghiii gimo ai baracconiiii");
+            vincitore = Thread.currentThread().getName();
+
         }else
             System.out.println("ho persooooooo adesso schiavizzo i paguri");
     }
